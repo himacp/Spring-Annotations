21f3c034b56a01d5ef4a3ba93ff5aa79ab01afb7
hima <himacp2396@gmail.com>
2019-01-16 15:05:56 +0530

    Constructor injection

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(Appconfig.class);
-        //context.register(Appconfig.class);
+        //context.register(Appconfig.class); No need of using this class if we declare earlier
         //context.refresh();
 
         Movie movie1=(Movie) context.getBean("movieA");
--- src/main/java/com/stackroute/config/Appconfig.java
+++ src/main/java/com/stackroute/config/Appconfig.java
@@ -45,16 +45,3 @@ public class Appconfig
     }
 }
 
-
-//package com.stackroute.config;
-//
-//import com.stackroute.domain.Actor;
-//import com.stackroute.domain.Movie;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@ComponentScan(basePackages = "com.stackroute")
-//public class Appconfig{
-//}
